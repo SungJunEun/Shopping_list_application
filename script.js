859fb165cec829665af175c2136cd8c36748cf6c
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-08-15 13:52:33 +0000

    wrote better way from the course

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const input = document.querySelector('input');
 const button = document.querySelector('#submit');
-
 const ul = document.querySelector('ul');
+/*
 let garbage = [];
 
 button.addEventListener('click', eventhandler1);
 input.addEventListener('keydown',eventhandler2);
+
 function eventhandler1() {
   displayitem();
 }
@@ -17,7 +18,7 @@ function eventhandler2(e) {
 function eventhandler3(e) {
   let parent = e.target.parentNode.parentNode;
   if (parent.tagName == 'UL' ) {
-    return;
+    parent = parent.firstChild;
   }
   console.log(parent.tagName);
   parent.remove();
@@ -32,12 +33,58 @@ function displayitem() {
             <button id= "garbage"><i class="fas fa-trash-alt"></i></button>
   `;
   ul.append(li);
-  event.currentTarget.value = "";
+  input.value = "";
+  input.focus;
   removeitem();
 }
 function removeitem() {
   let garbage = document.querySelectorAll('#garbage');
-  console.log(garbage);garbage.forEach(function(elem){
+  console.log(garbage);
+  garbage.forEach(function(elem){
   elem.addEventListener('click', eventhandler3);
 })
+}
+*/
+function onAdd() {
+  //1. 타이핑한 값 받아오기
+  const new_input = input.value;
+  //2. 새로운 아이템 만듦
+  const item = createItem(new_input);
+  //3. ul에 추가
+  ul.appendChild(item);
+  //4. 초기화
+  input.value = "";
+  input.focus();
+
+}
+function createItem(new_input) {
+  const item = document.createElement('li');
+  item.setAttribute('class', 'item');
+
+  const span = document.createElement('span');
+  span.setAttribute('class', 'text');
+  span.innerText = new_input;
+
+  const delete_button = document.createElement('button');
+  delete_button.setAttribute('id', 'garbage');
+  delete_button.innerHTML = `<i class="fas fa-trash-alt"></i>`
+  delete_button.addEventListener('click', ()=>{
+    delete_button.parentNode.remove();
+  })
+
+  item.appendChild(span);
+  item.appendChild(delete_button); 
+  return item;
+}
+
+button.addEventListener('click', eventhandler1);
+input.addEventListener('keydown',eventhandler2);
+function eventhandler1() {
+  onAdd();
+}
+
+function eventhandler2(e) {
+  if(e.keyCode === 13) {
+    onAdd();
+  }
 }
